73c0c913dce6e0725157c505dd42e7bb43b6b0ac
Arpit Jain <121026251+craj26@users.noreply.github.com>
2023-03-19 15:35:11 +0530

    f1

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
-function validEmail(str) {
+function validEmail(inputText) {
+  var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+if(inputText.value.match(mailformat))
+{
+alert("Valid email address!");
+document.form1.text1.focus();
+return true;
+}
+else
+{
+alert("You have entered an invalid email address!");
+document.form1.text1.focus();
+return false;
   //your JS code here.
 }
 
